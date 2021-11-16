6d4ef4e7f91ce416390795040eb66d49cfd6ce23
Phil <philip.b.rosen@gmail.com>
2021-11-15 19:11:18 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.write("This is my first test")
+st.write("Testing this as an update")
 st.sidebar.write("This is the sidebar")
 num_days=st.sidebar.slider('Number of days', 1,30,3)
 
